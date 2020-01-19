4e16f1d9bd40a70145640354dda6de24f2f7248f
Geoge <jkjkj.power@gmail.com>
2020-01-19 21:46:25 +0300

    added empty list image

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { View, Image, FlatList, StyleSheet } from "react-native";
 import Add from "../components/Add";
 import List from "../components/List";
 import { Methods } from "../../App";
@@ -15,21 +15,41 @@ const MainScreen = ({ addTask, list, deleteTask, onTaskPress }: Props) => {
   return (
     <View>
       <Add addTask={addTask} />
-      <FlatList
-        keyExtractor={item => item.id.toString()}
-        data={list}
-        renderItem={({ item }) => (
-          <List
-            deleteTask={deleteTask}
-            todo={item}
-            onTaskPress={() => onTaskPress(item.id)}
+      {list.length ? (
+        <FlatList
+          keyExtractor={item => item.id.toString()}
+          data={list}
+          renderItem={({ item }) => (
+            <List
+              deleteTask={deleteTask}
+              todo={item}
+              onTaskPress={() => onTaskPress(item.id)}
+            />
+          )}
+        />
+      ) : (
+        <View>
+          <Image
+            style={style.img}
+            source={require("../../assets/no-items.png")}
           />
-        )}
-      />
+        </View>
+      )}
     </View>
   );
 };
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  imgWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    height: 300
+  },
+  img: {
+    width: "100%",
+    resizeMode: "contain"
+  }
+});
 
 export default MainScreen;
